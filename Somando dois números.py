a85f97df7fea300313121f18f1632f48c6adb336
Otávio Araujo <51797012+otavioaraujo08@users.noreply.github.com>
2021-05-06 09:27:18 -0300

    Delete Somando dois números.py

--- Somando dois números.py
+++
@@ -1,4 +0,0 @@
-valor_um = int(input("Digite um valor para somar:\n "))
-valor_dois = int(input("Digite um valor para somar:\n "))
-
-print(f'A soma de {valor_um} com {valor_dois} eh {valor_um + valor_dois}.')
