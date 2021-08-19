87fdc4e748acdcf45d4bb27838a60fa2c6f1c556
Otávio Araujo <51797012+otavioaraujo08@users.noreply.github.com>
2021-08-19 08:07:57 -0300

    Delete From_Import directory

--- From_Import/From_Import.py
+++
@@ -1,7 +0,0 @@
-# Realizando a Importação Da Classe Carro
-from Carro import Carro
-
-carro_um = Carro("Chevrolet", "Esportivo", 2021, "Preto")
-
-print(f"O Carro: {carro_um.marca}\nModelo: {carro_um.modelo}.\nAno: {carro_um.ano}.\nCor: {carro_um.cor}")
-print(carro_um.dirigir())
